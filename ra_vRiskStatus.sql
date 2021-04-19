76451bdb0ce7b9b022b0177b94ec1accf1a65b82
carrie-tong <carrie.tong@telus.com>
2021-04-19 17:30:18 -0600

    Create TimeToCompleAssessment Calculation
    update DATEDIFF to calculate the time to complete assessment field (last update date - create date)

--- ra_vRiskStatus.sql
+++ ra_vRiskStatus.sql
@@ -1,17 +1,17 @@
 USE [ReliabilityAssessmentDb_DEV]
 GO
 
-DROP VIEW [ReliabilityAssessment].[vRiskStatus]
-GO
+--DROP VIEW [ReliabilityAssessment].[vRiskStatus]
+--GO
 
 CREATE VIEW [ReliabilityAssessment].[vRiskStatus]
 AS
 SELECT ReliabilityAssessment.Project.ProjectId AS ProjectID, ReliabilityAssessment.Project.Name AS ProjectName, 
        Definition.ProjectStatus.Name AS ProjectStatus, COALESCE(Definition.AssessmentRisk.Name, 'NULL') AS RiskLevel,
-       CAST(ReliabilityAssessment.Project.PlannedStartDate AS date) AS'PlannedStartDate',
-	   CAST(ReliabilityAssessment.Project.PlannedDeliveryDate AS date) AS'PlannedDeliveryDate',
-       CAST(ReliabilityAssessment.Project.CreateDateTimeOffset AS datetime) AS 'CreateDateTimeMT',
-	   CAST(ReliabilityAssessment.Project.LastUpdateDateTimeOffset AS datetime) AS 'LastUpdateDateTimeMT'
+
+	   CAST(ReliabilityAssessment.Project.LastUpdateDateTimeOffset AS date) AS 'LastUpdateDate',
+	   CAST(ReliabilityAssessment.Project.CreateDateTimeOffset AS date) AS 'CreateDate',
+	   DATEDIFF(day, ReliabilityAssessment.Project.CreateDateTimeOffset,ReliabilityAssessment.Project.LastUpdateDateTimeOffset) as TimeToCompletAssessment
 
 FROM   ReliabilityAssessment.Project
 		   INNER JOIN Definition.ProjectStatus 
@@ -28,6 +28,7 @@ WHERE (Definition.ProjectStatus.Name = 'Approved') OR
            (Definition.ProjectStatus.Name = 'Rejected') OR
            (Definition.ProjectStatus.Name = 'Stage 1 complete') OR
            (Definition.ProjectStatus.Name = 'Stage 2 complete')
+
 GO
 
 
