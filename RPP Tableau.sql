c3959a2cdc1a31cfaebb7e69ab3b72b4bbc0736c
carrie-tong <carrie.tong@telus.com>
2021-05-06 13:23:37 -0600

    Revised RPPid to varchar(10) instread of int

--- RPP Tableau.sql
+++ RPP Tableau.sql
@@ -1,4 +1,5 @@
-select  t.projectid as ProejctID, cast(tp.createdon as date) as CreateDate, t. fdleadnetops as Manager, t.englvlnetops as EngagementLevel, t.nextstepnetops as RPPStatus
+select  cast(t.projectid as varchar(10)) as RppID, cast(tp.createdon as date) as CreateDate, t. fdleadnetops as NetOpsLead, t.englvlnetops as EngagementLevel, t.nextstepnetops as RPPStatus,
+dateadd(day, -datepart(day, cast(tp.createdon as date))+1, cast(tp.createdon as date)) as Month
 from [dbo].[vw_tefd_projects_details] t left join [dbo].[vw_tefd_projects] tp
 on t.projectid = tp.id
 where t. fdleadnetops like 'Stuart%' and (t.englvlnetops like 'Standard%' or t.englvlnetops like  'high%')
