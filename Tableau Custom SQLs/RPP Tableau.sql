9b21ee95ad60fdd881e432c07b924259f7018124
carrie-tong <75696798+carrie-tong@users.noreply.github.com>
2021-06-02 18:49:34 -0600

    PPR

--- Tableau Custom SQLs/RPP Tableau.sql
+++ Tableau Custom SQLs/RPP Tableau.sql
@@ -1,7 +1,8 @@
-select  cast(t.projectid as varchar(10)) as RppID, cast(tp.createdon as date) as CreateDate, t. fdleadnetops as NetOpsLead, t.englvlnetops as EngagementLevel, t.nextstepnetops as RPPStatus,
-        t. fdleadrel as Leadrel, t.englvlrel as EngagementLevelrel, t.nextsteprel as RPPStatusrel,
-        dateadd(day, -datepart(day, cast(tp.createdon as date))+1, cast(tp.createdon as date)) as Month, 
-		tp.createdby as Submitter, tp.leadpm as [Lead PM]
+select  cast(t.projectid as varchar(10)) as RppID, cast(tp.createdon as date) as CreateDate, 
+        t. fdleadnetops as [NetOps Lead], t.englvlnetops as [NetOps Engagement Level], t.nextstepnetops as RPPStatus,
+        t. fdleadrel as [Reliability Lead], t.englvlrel as [Reliability Engagement Level], t.nextsteprel as [Reliability RPP Status],
+        dateadd(day, -datepart(day, cast(tp.createdon as date))+1, cast(tp.createdon as date)) as Month, tp.submitter_name as Submitter
+		---t.primenetops as [NetOps Prime]
 from [dbo].[vw_tefd_projects_details] t left join [dbo].[vw_tefd_projects] tp
 on t.projectid = tp.id
 where (t. fdleadnetops like 'Stuart%' and (t.englvlnetops like 'Standard%' or t.englvlnetops like  'high%')
