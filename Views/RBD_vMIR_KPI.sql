6dc1eaa7e5d2539a9361874e016788858d779cf4
carrie-tong <75696798+carrie-tong@users.noreply.github.com>
2021-06-15 14:29:04 -0600

    Update Target in MIR

--- Views/RBD_vMIR_KPI.sql
+++ Views/RBD_vMIR_KPI.sql
@@ -4,7 +4,7 @@ select @startdate = '20210101', @enddate = getdate()
 
 select count(distinct(mirnumber)) as KPI,
 dateadd(day, -datepart(day, cast(m.ImpactMonthDate as date))+1, cast(m.ImpactMonthDate as date)) as MonthYear,
-'Total # design-related incidents' as KPIName
+'Total # design-related incidents' as KPIName, 40 as Target
 from [NuRemedy].[METRICS_MIR].[RBD_vDesignRelatedImpact] m
 where ImpactMonthDate >= @startdate and ImpactMonthDate<=@enddate
 group by m.ImpactMonthDate
@@ -12,7 +12,7 @@ group by m.ImpactMonthDate
 union
 select avg(impactcount) as KPI,
 dateadd(day, -datepart(day, cast(m.ImpactMonthDate as date))+1, cast(m.ImpactMonthDate as date)) as MonthYear,
-'Average # customers impacted by design' as KPIName
+'Average # customers impacted by design' as KPIName, 350 as Target
 from [NuRemedy].[METRICS_MIR].[RBD_vDesignRelatedImpact] m
 where ImpactMonthDate >= @startdate and ImpactMonthDate<=@enddate
 group by m.ImpactMonthDate
@@ -20,7 +20,7 @@ group by m.ImpactMonthDate
 union
 select count(distinct case when impactcount>1000 then mirnumber else null end) as KPI,
 dateadd(day, -datepart(day, cast(m.ImpactMonthDate as date))+1, cast(m.ImpactMonthDate as date)) as MonthYear,
-'Total # design-related incidents with customer impact >1000' as KPIName
+'Total # design-related incidents with customer impact >1000' as KPIName, 5 as Target
 from [NuRemedy].[METRICS_MIR].[RBD_vDesignRelatedImpact] m
 where ImpactMonthDate >= @startdate and ImpactMonthDate<=@enddate
 group by m.ImpactMonthDate
